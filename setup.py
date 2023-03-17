5b75b8ce8af5476cd78c4154cdd1e393af86f2e7
FernandoCelmer <email@fernandocelmer.com>
2023-03-16 22:36:36 -0300

    ⚙️  NO-TASK - Update Version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'install': CustomInstallCommand,
     },
     install_requires=[
-        'linuxp>=1.0.14'
+        'linuxp>=1.0.15'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
