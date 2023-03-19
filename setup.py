c1111b977d26f32beb0d8b4f79e50519b8fe53a3
FernandoCelmer <email@fernandocelmer.com>
2023-03-18 22:10:18 -0300

    📦 PyPI - Update Settings

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     description="🐧 Plugin Linux Profile",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/MyLinuxProfile/linux-profile",
+    url="https://github.com/MyLinuxProfile/linux-profile-plugin",
     cmdclass={
         'install': CustomInstallCommand,
     },
